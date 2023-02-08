c130a13bcf90f2d5b8be83fd67c788680ee1d43a
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-02-08 10:45:38 +0100

    Ajout dans la liste de pokemons et affichage de la liste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+type PokemonGender = 'male' | 'female' | 'no gender';
+
+interface Pokemon {
+  name: string;
+  gender: PokemonGender;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'promo-septembre-2022-angular-essentials';
+  currentPokemonName = '';
+  pokemons: Pokemon[] = [];
+
+  addPokemon() {
+    this.pokemons.push({
+      name: this.currentPokemonName,
+      gender: this.getRandomGender(),
+    });
+  }
+
+  getRandomGender(): PokemonGender {
+    const rand = Math.random();
+    if (rand < 0.3) return 'male';
+    if (rand < 0.6) return 'female';
+    return 'no gender';
+  }
 }
