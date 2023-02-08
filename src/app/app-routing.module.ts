01c2c57f266ae188dc2404ae81eeb9c845adf3d1
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-02-08 16:29:49 +0100

    Séparation du AppRoutingModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { DetailsComponent } from './pages/pokemon/details/details.component';
+import { GeneralComponent } from './pages/pokemon/general/general.component';
+import { PokemonComponent } from './pages/pokemon/pokemon.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'pokemon/:index',
+    component: PokemonComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'general',
+        pathMatch: 'full',
+      },
+      {
+        path: 'general',
+        component: GeneralComponent,
+      },
+      {
+        path: 'details',
+        component: DetailsComponent,
+      },
+    ],
+  },
+  {
+    path: 'not-found',
+    component: PageNotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,46 +5,12 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { PokemonItemComponent } from './components/pokemon-item/pokemon-item.component';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PokemonComponent } from './pages/pokemon/pokemon.component';
 import { GeneralComponent } from './pages/pokemon/general/general.component';
 import { DetailsComponent } from './pages/pokemon/details/details.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'pokemon/:index',
-    component: PokemonComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'general',
-        pathMatch: 'full',
-      },
-      {
-        path: 'general',
-        component: GeneralComponent
-      },
-      {
-        path: 'details',
-        component: DetailsComponent
-      }
-    ]
-  },
-  {
-    path: 'not-found',
-    component: PageNotFoundComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'not-found',
-  },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -60,7 +26,7 @@ const routes: Routes = [
     BrowserModule,
     NgbModule,
     FormsModule,
-    RouterModule.forRoot(routes),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
