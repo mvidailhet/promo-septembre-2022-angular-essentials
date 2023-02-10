7baf54e1f08996045521b19a8d762984c3ddf947
Michel Vidailhet <michel.vidailhet@gmail.com>
2023-02-10 14:30:06 +0100

    async validator

--- src/app/pages/login-model-driven/login-model-driven.component.ts
+++ src/app/pages/login-model-driven/login-model-driven.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { from, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-login-model-driven',
@@ -9,11 +10,12 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class LoginModelDrivenComponent {
   loginForm: FormGroup;
   forbiddenPasswords = ['1234', 'azerty'];
+  forbiddenEmails = ['toto@gmail.com'];
 
   constructor() {
     this.loginForm = new FormGroup({
       userData: new FormGroup({
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmailValidator.bind(this)),
         password: new FormControl(null, [Validators.required, this.forbiddenPasswordsValidator.bind(this)]),
       }),
       comment: new FormControl(null),
@@ -25,6 +27,19 @@ export class LoginModelDrivenComponent {
     return {
       forbidden: true
     };
+  }
+
+  forbiddenEmailValidator(control: AbstractControl): Observable<{ forbidden: boolean } | null> {
+    const promise = new Promise<{ forbidden: boolean } | null>((resolve) => {
+      setTimeout(() => {
+        if (!this.forbiddenEmails.includes(control.value)) {
+          resolve(null);
+          return;
+        }
+        resolve({ forbidden: true });
+      }, 2000);
+    });
+    return from(promise);
   }
 
   onSubmit() {
